0a15c3535572dbbe8290338842cf832d9aec8883
Berat Zengin <devberatzengin@gmail.com>
2026-02-22 12:52:43 +0300

    refactor: add Token and TokenData schemas

--- app/schemas/user_sh.py
+++ app/schemas/user_sh.py
@@ -28,3 +28,11 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    user_id: Optional[str] = None
